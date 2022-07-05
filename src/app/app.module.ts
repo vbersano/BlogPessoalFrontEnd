c4d2646d55e0c4367be6238ccee29eb3a44c2608
Vinicius Bersano <tk3.bersano@hotmail.com>
2022-07-04 23:41:54 -0300

    footer-and-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
